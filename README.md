7887bf86dc3f77e605352a1e77458d5123566c4d
makerio90 <makerio1@pm.me>
2022-04-19 16:08:24 -0400

    main

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is year_progress, the cool project made by makerio
+## installation instructions
+simply run `cargo install --git www.github.com/makerio/year_progress`
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use chrono::prelude::*;
+use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
+/// Simple program to show the progress of time
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// start date (%Y-%m-%d %H:%M:%S)
+    #[clap(short, long)]
+    start: Option<String>,
+
+    /// end date (%Y-%m-%d %H:%M:%S)
+    #[clap(short, long)]
+    end: Option<String>,
+
+    /// loop until end date reached
+    #[clap(short)]
+    l: bool,
+
+    ///bar thickness
+    #[clap(short, long)]
+    thickness: u32,
+}
+
+fn main() {
+    let args = Args::parse();
+    // times
+    let mut now = Utc::now();
+    let start = match args.start {
+        Some(s) => DateTime::<Utc>::from_utc(
+            NaiveDateTime::parse_from_str(&s, "%Y-%m-%d %H:%M:%S").unwrap(),
+            Utc,
+        ),
+        None => Utc.ymd(now.year(), 1, 1).and_hms(0, 0, 0),
+    };
+    let end = match args.end {
+        Some(s) => DateTime::<Utc>::from_utc(
+            NaiveDateTime::parse_from_str(&s, "%Y-%m-%d %H:%M:%S").unwrap(),
+            Utc,
+        ),
+        None => Utc.ymd(now.year(), 12, 31).and_hms(12, 59, 59),
+    };
+    // math
+    let total_seconds = (end.timestamp() - start.timestamp()) as u64;
+    // bar
+    let bar = ProgressBar::new(total_seconds);
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template(&format!("[{{bar:{}}}] {{msg}}", args.thickness))
+            .progress_chars("=> "),
+    );
+    //bar.set_draw_delta(total_seconds / 100);
+    loop {
+        now = Utc::now();
+        let seconds_ran = (now.timestamp() - start.timestamp()) as u64;
+        let progress: f64 = (seconds_ran as f64 / total_seconds as f64) * 100.0;
+        if progress > 100.0 {
+            break;
+        }
+        bar.set_position(seconds_ran);
+        bar.set_message(format!("{}%", progress));
+        if !args.l {
+            break;
+        };
+    }
+    bar.finish_at_current_pos();
+}
